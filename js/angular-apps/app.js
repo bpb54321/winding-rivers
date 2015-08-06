732f1c4d54a42997c556da9f34b2042985948a30
Brian Blosser <bpb54321@gmail.com>
2015-08-06 11:59:36 -0500

    fin model page: added Bootstrap list group and tabs and desc for model types

--- js/angular-apps/app.js
+++ js/angular-apps/app.js
@@ -1,4 +1,35 @@
 
-var app = angular.module("FinancialModelApp",[]);
+(function() {
+    var app = angular.module("FinancialModelApp",[]);
+    
+    app.controller('DisplayBoxController', function() {
+        this.annualIncome = 30000;
+        this.costOfProperty = 100000;
+        this.numOfUnits = 12;
+        //["Split Evenly","Sliding Scale"];
+        this.costSharingModel = [
+            {
+                name:"Split Evenly",
+                desc:"In this model, the group mortgage is split evenly " +
+                        "by all members, regardless of their incomes."
+        },
+            {
+                name:"Sliding Scale",
+                desc:"In this model, the member's fraction of the group mortgage " +
+                        "is proportional to the member's fraction of the group's " +
+                        "combined income."
+            
+        }];
+    });
+    app.controller('ModelController', function() {
+        this.activeTab = 1;
+        this.setTab = function(selectedTab) {
+            this.activeTab = selectedTab;
+        };
+        this.isTabActive = function(tabInQuestion) {
+            return this.activeTab === tabInQuestion;
+        };
+    });
+})();
 
 
