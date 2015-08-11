1c0bd266a86824e1a194824e36d959965b25ee22
Brian Blosser <bpb54321@gmail.com>
2015-08-11 15:40:13 -0500

    Displaying table, incomeData is in separate object.

--- js/angular-apps/app.js
+++ js/angular-apps/app.js
@@ -2,6 +2,20 @@
 (function() {
     var app = angular.module("mainApp",[]);
     
+    var stlIncomes2013 = {
+        incomeGroups:["<10k","10k-20k","20k-30k","30k-40k","40k-50k","50k-60k","60k-75k",
+            "75k-100k","100k-125k","125k-150k","150-200k",">200k"],
+        representativeIncomes: [10000,15000,25000,35000,45000,55000,67500,87500,112500,
+            137500,175000,200000],
+        distributionPct: [16,15,14,11,10,7,8,8,5,2,2,2],
+        peoplePerIncome: [0,0,0,0,0,0,0,0,0,0,0,0],
+        calcPeoplePerIncome: function(groupSize) {
+            for (var i=0; i++; i<this.representativeIncomes.length) {
+                this.peoplePerIncome[i] = this.distributionPct[i]*.01*groupSize;
+            }
+        }
+    };
+    
     app.controller('DisplayBoxController', function() {
         this.annualIncome = 30000;
         this.costOfProperty = 100000;
@@ -9,6 +23,7 @@
         this.costSharingModel = "Split Evenly";
         this.monthlyPayment = 0;
         this.apr = 4;
+        this.incomeData=stlIncomes2013;
         this.calcMonthlyPayment = function() {
             if (this.costSharingModel==="Split Evenly") {
                 //this.monthlyPayment = this.costOfProperty/this.numOfUnits;
@@ -18,6 +33,7 @@
                 var onePersonPayment= (this.costOfProperty * rate) / (1 - Math.pow(1+rate,-months));
                 this.monthlyPayment = onePersonPayment/this.numOfUnits;
             } else {
+                this.incomeData.calcPeoplePerIncome(this.numOfUnits);
                 this.monthlyPayment = -3;
             }
              
@@ -36,15 +52,15 @@
 //            
 //        }];
     });
-    app.controller('ModelController', function() {
-        this.activeTab = 1;
-        this.setTab = function(selectedTab) {
-            this.activeTab = selectedTab;
-        };
-        this.isTabActive = function(tabInQuestion) {
-            return this.activeTab === tabInQuestion;
-        };
-    });
+//    app.controller('ModelController', function() {
+//        this.activeTab = 1;
+//        this.setTab = function(selectedTab) {
+//            this.activeTab = selectedTab;
+//        };
+//        this.isTabActive = function(tabInQuestion) {
+//            return this.activeTab === tabInQuestion;
+//        };
+//    });
 })();
 
 
