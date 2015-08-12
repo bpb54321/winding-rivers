12c5f7567ff42ff9e2e5cbf75710438275d6056c
Brian Blosser <bpb54321@gmail.com>
2015-08-12 11:20:21 -0500

    Logic of app is fully tested and functional!

--- js/angular-apps/app.js
+++ js/angular-apps/app.js
@@ -1,42 +1,67 @@
 
-(function() {
-    var app = angular.module("mainApp",[]);
-    
-    var stlIncomes2013 = {
-        incomeGroups:["<10k","10k-20k","20k-30k","30k-40k","40k-50k","50k-60k","60k-75k",
-            "75k-100k","100k-125k","125k-150k","150-200k",">200k"],
-        representativeIncomes: [10000,15000,25000,35000,45000,55000,67500,87500,112500,
-            137500,175000,200000],
-        distributionPct: [16,15,14,11,10,7,8,8,5,2,2,2],
-        peoplePerIncome: [0,0,0,0,0,0,0,0,0,0,0,0],
-        calcPeoplePerIncome: function(groupSize) {
-            for (var i=0; i++; i<this.representativeIncomes.length) {
-                this.peoplePerIncome[i] = this.distributionPct[i]*.01*groupSize;
-            }
-        }
-    };
-    
-    app.controller('DisplayBoxController', function() {
+(function () {
+    var app = angular.module("mainApp", []);
+
+//    var stlIncomes2013 = {
+//        incomeGroups:["<10k","10k-20k","20k-30k","30k-40k","40k-50k","50k-60k","60k-75k",
+//            "75k-100k","100k-125k","125k-150k","150-200k",">200k"],
+//        representativeIncomes: [10000,15000,25000,35000,45000,55000,67500,87500,112500,
+//            137500,175000,200000],
+//        distributionPct: [16,15,14,11,10,7,8,8,5,2,2,2],
+//        peoplePerIncome: [0,0,0,0,0,0,0,0,0,0,0,0],
+//        calcPeoplePerIncome: function(groupSize) {
+//            for (var i=0; i++; i<this.representativeIncomes.length) {
+//                this.peoplePerIncome[i] = this.distributionPct[i]*.01*groupSize;
+//            }
+//        }
+//    };
+
+    app.controller('DisplayBoxController', function () {
         this.annualIncome = 30000;
         this.costOfProperty = 100000;
         this.numOfUnits = 12;
         this.costSharingModel = "Split Evenly";
         this.monthlyPayment = 0;
-        this.apr = 4;
-        this.incomeData=stlIncomes2013;
-        this.calcMonthlyPayment = function() {
-            if (this.costSharingModel==="Split Evenly") {
-                //this.monthlyPayment = this.costOfProperty/this.numOfUnits;
-                var years=30;
-                var months=years*12;
-                var rate=(this.apr/12)/100; //monthly interest rate in decimal form
-                var onePersonPayment= (this.costOfProperty * rate) / (1 - Math.pow(1+rate,-months));
-                this.monthlyPayment = onePersonPayment/this.numOfUnits;
-            } else {
-                this.incomeData.calcPeoplePerIncome(this.numOfUnits);
-                this.monthlyPayment = -3;
+        this.apr = 5;
+        this.incomeGroups = ["<10k", "10k-20k", "20k-30k", "30k-40k", "40k-50k", "50k-60k", "60k-75k",
+            "75k-100k", "100k-125k", "125k-150k", "150-200k", ">200k"];
+        this.representativeIncomes = [10000, 15000, 25000, 35000, 45000, 55000, 67500, 87500, 112500,
+            137500, 175000, 200000];
+        this.distributionPct = [16, 15, 14, 11, 10, 7, 8, 8, 5, 2, 2, 2];
+        this.peoplePerIncome = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.totalGroupAnnualIncome = 0;
+        this.pctIncomeTowardsMortgage = 0;
+        this.groupMonthlyMortgagePayment = 0;
+        this.groupAnnualMortgagePayment = 0;
+        this.calcPeoplePerIncome = function (groupSize) {
+            for (var i = 0; i < this.representativeIncomes.length; i++) {
+                this.peoplePerIncome[i] = this.distributionPct[i] * .01 * groupSize;
             }
-             
+        };
+        //this.incomeData=stlIncomes2013;
+
+        this.calcMonthlyPayment = function () {
+            var years = 30;
+            var months = years * 12;
+            var rate = (this.apr / 12) / 100; //monthly interest rate in decimal form
+            this.groupMonthlyMortgagePayment = (this.costOfProperty * rate) / (1 - Math.pow(1 + rate, -months));
+            this.groupAnnualMortgagePayment = this.groupMonthlyMortgagePayment * 12;
+
+
+            if (this.costSharingModel === "Split Evenly") {
+                this.monthlyPayment = this.groupMonthlyMortgagePayment / this.numOfUnits;
+            } else {
+                this.totalGroupAnnualIncome = 0; //reset total group income
+                this.calcPeoplePerIncome(this.numOfUnits);
+                for (var j = 0; j < this.peoplePerIncome.length; j++) {
+                    this.totalGroupAnnualIncome += this.peoplePerIncome[j] *
+                            this.representativeIncomes[j];
+                }
+                this.pctIncomeTowardsMortgage = (this.groupAnnualMortgagePayment / this.totalGroupAnnualIncome) * 100;
+
+                this.monthlyPayment = (this.annualIncome * this.pctIncomeTowardsMortgage * .01)/12;
+            }
+
         };
 //        this.costSharingModel = [
 //            {
