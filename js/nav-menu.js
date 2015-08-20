3319d8a5784a019cd7b4772dce7b900227ed6f5a
Brian Blosser <bpb54321@gmail.com>
2015-08-20 14:33:28 -0500

    Menu slides in and out on index.html.

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -5,7 +5,16 @@ $(document).ready(function () {
             function () {
                 $('.nav').animate({
                     top: "0px"
-                }, 500, function () {
+                }, 200, function () {
+                    // Animation complete, do something.
+                });
+            }
+    );
+    $('.fa-angle-up').click(
+            function () {
+                $('.nav').animate({
+                    top: "-25px"
+                }, 200, function () {
                     // Animation complete, do something.
                 });
             }
